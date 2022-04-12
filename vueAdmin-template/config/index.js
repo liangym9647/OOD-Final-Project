655b7e44edebce2f00e69d2dcd1b349fdff20649
liangym9647 <liangym96@gwu.edu>
2022-04-12 19:57:19 -0400

    the final project has been finished

--- vueAdmin-template/config/index.js
+++ vueAdmin-template/config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     proxyTable: {
       '/api': {
         //target: IPv4 address
-        target: 'http://10.0.0.54:8080',//要跨域的域名
+        target: 'http://10.198.51.120:8080',//要跨域的域名
         secure: false, //如果是https接口，如要配置此参数
         changeOrigin: true,//允许跨域
         pathRewrite: {
